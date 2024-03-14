42735f2a18555347260301f67a080d20f0d7f3e4
aneisner <antonio.eisner@suse.com>
2024-03-14 12:52:08 +0100

    add some commands and options in cli

--- src/wt-cli.py
+++ src/wt-cli.py
@@ -18,20 +18,32 @@ def main():
     subparsers = parser.add_subparsers(help='sub-command help')
 
     # create the parser for the "list" command
-    parser_a = subparsers.add_parser('list', help='a help')
-    parser_a.add_argument('bar', type=int, help='bar help')
+    parser_a = subparsers.add_parser('list', help='list help')
+    parser_a.add_argument('--all', action='store_true', help='all help')
+    parser_a.add_argument('--day', help='day help')
 
     # create the parser for the "add" command
-    parser_b = subparsers.add_parser('add', help='b help')
-    parser_b.add_argument('--baz', choices='XYZ', help='baz help')
+    parser_b = subparsers.add_parser('add', help='add help')
+    parser_b.add_argument('--day', help='day help')
+    parser_b.add_argument('--start-time', help='start-time help')
+    parser_b.add_argument('--end-time', help='end-time help')
+    parser_b.add_argument('--break-time', help='breaktime help')
+    parser_b.add_argument('--comment', help='comment help')
 
     # create the parser for the "edit" command
-    parser_c = subparsers.add_parser('edit', help='help me')
-    # parse some argument lists
+    parser_c = subparsers.add_parser('edit', help='list help')
+    parser_c.add_argument('--day', help='day help')
+    parser_c.add_argument('--start-time', action='store_true', help='start-time help')
+    parser_c.add_argument('--end-time', action='store_true', help='end-time help')
+    parser_c.add_argument('--break-time', action='store_true', help='breaktime help')
+    parser_c.add_argument('--comment', action='store_true', help='comment help')
 
     # create the parser for the "del" command
-    parser_b = subparsers.add_parser('del', help='b help')
-    parser_b.add_argument('--baz', choices='XYZ', help='baz help')
+    parser_d = subparsers.add_parser('del', help='del help')
+    parser_d.add_argument('--day', help='day help')
+    parser_d.add_argument('--reset-repo', help='reset-repo help')
+    parser_d.add_argument('--baz', choices='XYZ', help='baz help')
+
     args = parser.parse_args()
 
     print(args.foo)
