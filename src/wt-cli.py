b042d998c942eb8532432d8c1ccda5804d3ab29d
aneisner <antonio.eisner@suse.com>
2024-03-14 09:41:50 +0100

    update working on cli

--- src/wt-cli.py
+++ src/wt-cli.py
@@ -17,18 +17,23 @@ def main():
     parser.add_argument('--bar', help='foo help')
     subparsers = parser.add_subparsers(help='sub-command help')
 
-    # create the parser for the "a" command
+    # create the parser for the "list" command
     parser_a = subparsers.add_parser('list', help='a help')
     parser_a.add_argument('bar', type=int, help='bar help')
 
-    # create the parser for the "b" command
+    # create the parser for the "add" command
     parser_b = subparsers.add_parser('add', help='b help')
     parser_b.add_argument('--baz', choices='XYZ', help='baz help')
 
+    # create the parser for the "edit" command
     parser_c = subparsers.add_parser('edit', help='help me')
     # parse some argument lists
 
+    # create the parser for the "del" command
+    parser_b = subparsers.add_parser('del', help='b help')
+    parser_b.add_argument('--baz', choices='XYZ', help='baz help')
     args = parser.parse_args()
+
     print(args.foo)
     print(args.bar)
     #wt_options(args)
