bfa7e3dd0da8990f25a9b491a29dd68099ae9205
aneisner <antonio.eisner@suse.com>
2024-03-14 12:54:05 +0100

    Add Example for argpaser

---
+++ testfiles/wt-cli.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3.6
+
+import argparse
+
+def wt_options(args):
+    #workinghours list
+    if args.list and args.all:
+        print("list + all")
+    if args.list and args.day:
+        print("list + day")
+
+
+def main():
+    # create the top-level parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--foo', action='store_true', help='foo help')
+    parser.add_argument('--bar', help='foo help')
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    # create the parser for the "a" command
+    parser_a = subparsers.add_parser('list', help='a help')
+    parser_a.add_argument('bar', type=int, help='bar help')
+
+    # create the parser for the "b" command
+    parser_b = subparsers.add_parser('add', help='b help')
+    parser_b.add_argument('--baz', choices='XYZ', help='baz help')
+
+    parser_c = subparsers.add_parser('edit', help='help me')
+    # parse some argument lists
+
+    args = parser.parse_args()
+    print(args.foo)
+    print(args.bar)
+    #wt_options(args)
+
+if __name__ == "__main__":
+    main()
